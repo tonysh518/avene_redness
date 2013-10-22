cde04ce825231ad25f8ffdbb2b5d55fe29f77479
tonysh518 <tony@fuel-it-up.com>
2013-10-23 00:30:50 +0800

    update

--- js/app.js
+++ js/app.js
@@ -1,54 +1,66 @@
+$(document).ready(function(){
 
-var isUglyIe = $.browser.msie && $.browser.version <= 8;
-var isMostUglyIe = $.browser.msie && $.browser.version <= 6 ;
-/* for animation */
-var ANIMATE_NAME = "data-animate";
-$('[' + ANIMATE_NAME + ']')
-    .each(function(){
-        var $dom = $(this);
-        var tar = $dom.data('animate');
-        var style = $dom.data('style');
-        var time = parseInt( $dom.data('time') );
-        var delay = $dom.data('delay') || 0;
-        var easing = $dom.data('easing');
-        var begin = $dom.data('begin');
-        tar = tar.split(';');
-        var tarCss = {} , tmp;
-        for (var i = tar.length - 1; i >= 0; i--) {
-            tmp = tar[i].split(':');
-            if( tmp.length == 2 )
-                tarCss[ tmp[0] ] = $.trim(tmp[1]);
-        }
-        if( isUglyIe && tarCss.opacity !== undefined ){
-            delete tarCss.opacity;
-        }
+    var isUglyIe = $.browser.msie && $.browser.version <= 8;
+    var isMostUglyIe = $.browser.msie && $.browser.version <= 6 ;
 
-        style = style.split(';');
-        var styleCss = {} , tmp;
-        for (var i = style.length - 1; i >= 0; i--) {
-            tmp = style[i].split(':');
-            if( tmp.length == 2 )
-                styleCss[ tmp[0] ] = $.trim(tmp[1]);
-        }
-        if( isUglyIe && tarCss.opacity !== undefined ){
-            delete tarCss.opacity;
-        }
-        $dom.css(styleCss).delay( delay )
-            .animate( tarCss , time , easing );
-        if( begin ){
-            setTimeout(function(){
-                animation_begins[begin].call( $dom );
-            } , delay);
+    $(document.body).queryLoader2({
+        onLoading : function( percentage ){
+            console.log(percentage);
+        },
+        onComplete : function(){
+            $('.loading-wrap').fadeOut();
+            /* for animation */
+            var ANIMATE_NAME = "data-animate";
+            $('[' + ANIMATE_NAME + ']')
+                .each(function(){
+                    var $dom = $(this);
+                    var tar = $dom.data('animate');
+                    var style = $dom.data('style');
+                    var time = parseInt( $dom.data('time') );
+                    var delay = $dom.data('delay') || 0;
+                    var easing = $dom.data('easing');
+                    var begin = $dom.data('begin');
+                    tar = tar.split(';');
+                    var tarCss = {} , tmp;
+                    for (var i = tar.length - 1; i >= 0; i--) {
+                        tmp = tar[i].split(':');
+                        if( tmp.length == 2 )
+                            tarCss[ tmp[0] ] = $.trim(tmp[1]);
+                    }
+                    if( isUglyIe && tarCss.opacity !== undefined ){
+                        delete tarCss.opacity;
+                    }
+
+                    style = style.split(';');
+                    var styleCss = {} , tmp;
+                    for (var i = style.length - 1; i >= 0; i--) {
+                        tmp = style[i].split(':');
+                        if( tmp.length == 2 )
+                            styleCss[ tmp[0] ] = $.trim(tmp[1]);
+                    }
+                    if( isUglyIe && tarCss.opacity !== undefined ){
+                        delete tarCss.opacity;
+                    }
+                    $dom.css(styleCss).delay( delay )
+                        .animate( tarCss , time , easing );
+                    if( begin ){
+                        setTimeout(function(){
+                            animation_begins[begin].call( $dom );
+                        } , delay);
+                    }
+                });
         }
     });
 
 
-skrollr.init({
-    smoothScrollingDuration: 600,
-    smoothScrolling:true,
-    easing: 'easeInOutQuart'
-});
 
-$(window).scroll(function(){
-    console.log($(window).scrollTop());
+    skrollr.init({
+        smoothScrollingDuration: 600,
+        smoothScrolling:true,
+        easing: 'easeInOutQuart'
+    });
+
+    $(window).scroll(function(){
+        //console.log($(window).scrollTop());
+    });
 });
