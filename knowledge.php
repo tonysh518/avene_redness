e7c983cd74705846df5a5d2057dfbdd5eac068e2
nonozx <zx0110@gmail.com>
2013-10-24 02:39:47 +0800

    repair

--- knowledge.php
+++ knowledge.php
@@ -10,6 +10,7 @@
     <div class="knowledge_hua2"></div>
     <div class="knowledge_hua3"></div>
     <!--  -->
+    <div class="knowledge_img1 kl_movietit"></div>
     <div class="knowledge_img1 kl_moviebg"></div>
     <iframe frameborder="0" scrolling="no" class="knowledge_img1 kl_movie" id="video_iframe" src="video.php?id=1"></iframe>
     <div class="knowledge_img1 kl_movieshare"></div>
@@ -19,10 +20,16 @@
     <!--  -->
     <div class="knowledge_img2 kl_next"></div>
     <div class="knowledge_img2 kl_shenxin" data-0="opacity:0;margin-top:-50px;" data-400="opacity:1;margin-top:0;"></div>
-    <div class="knowledge_img2 kl_sxaq" data-100="opacity:0;margin-top:-50px;" data-500="opacity:1;margin-top:0;"></div>
+    <div class="knowledge_img2 kl_sxaq" data-100="opacity:0;margin-top:-50px;" data-500="opacity:1;margin-top:0;">
+        <div class="kl_q">我的皮肤平时不是很敏感，但是最近几个月开始使用一种去角质产品做深度清洁，但每次做完两颊都变得很红，有时还痛。请问我这样的肌肤是否是敏感造成的? 应该怎样预防敏感发生?</div>
+        <div class="kl_a">很遗憾，你应该是已经被敏感症状攻占了部分肌肤堡垒。频繁使用去角质产品会让你的角质层变得非常薄，因此才会突显出敏感和泛红。可以用柔和型护肤品，尽量少去角质，否则只会让脆弱的毛细血管更易破裂，敏感泛红更严重。</div>
+    </div>
     <iframe class="knowledge_img2 kl_sxweibo" data-100="opacity:0;margin-top:-50px;" data-500="opacity:1;margin-top:0;" width="63" scrolling="no" height="24" frameborder="0" allowtransparency="true" marginwidth="0" marginheight="0" border="0" src="http://widget.weibo.com/relationship/followbutton.php?language=zh_cn&amp;width=63&amp;height=24&amp;uid=2166726834&amp;style=1&amp;btn=red&amp;dpc=1"></iframe>
     <div class="knowledge_img2 kl_xier" data-0="opacity:0;margin-top:-50px;" data-400="opacity:1;margin-top:0;"></div>
-    <div class="knowledge_img2 kl_xieraq" data-100="opacity:0;margin-top:-50px;" data-500="opacity:1;margin-top:0;"></div>
+    <div class="knowledge_img2 kl_xieraq" data-100="opacity:0;margin-top:-50px;" data-500="opacity:1;margin-top:0;">
+        <div class="kl_q">冬季空气湿度较低,这个时候肌肤就会出现干燥脱皮的情况,更有可能因为过敏引起肌肤的泛红灼热。冬季美肤应该怎么吃? </div>
+        <div class="kl_a">其实女性的皮肤80%都是由胶原蛋白组成。胶原蛋白不仅保持肌肤弹性，更可以保持肌肤的锁水度，燕窝、猪脚等富含胶原蛋白的食品是很好的选择。菠菜、猪肝等含有大量的维生素A的食品也能帮助肌肤缓解干燥和敏感。而动物性如鱼、蛋、牛奶等、植物性如菠萝、植物花蕾等都容易导致皮肤过敏，所以要注意避免食用哦。</div>
+    </div>
     <!--  -->
     <div class="knowledge_img3 kl_methodtit" data-450="opacity:0;left:40%;" data-650="opacity:1;left:50%;">
         <h3 class="tit"></h3>
