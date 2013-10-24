5092ee1edcc4593e7ca28a6b820466623c504a55
nonozx <zx0110@gmail.com>
2013-10-24 19:01:39 +0800

    scheme

--- scheme.php
+++ scheme.php
@@ -21,6 +21,10 @@
     <div class="scheme_imgleft scheme_line scheme_line2" data-style="opacity:0;" data-animate="opacity:1;" data-delay="5400" data-time="1000" data-easing="easeInOutQuart"></div>
     <div class="scheme_imgleft scheme_line scheme_line3" data-style="opacity:0;" data-animate="opacity:1;" data-delay="5600" data-time="1000" data-easing="easeInOutQuart"></div>
     <div class="scheme_imgleft scheme_line scheme_line4" data-style="opacity:0;" data-animate="opacity:1;" data-delay="5800" data-time="1000" data-easing="easeInOutQuart"></div>
+    <!--  -->
+    <p class="dis_mobile arrow_tiem arrow_p"></p>
+    <p class="dis_mobile arrow_tiem arrow_n"></p>
+    <!--  -->
     <div class="scheme_imgleft scheme_step1" data-style="opacity:0;" data-animate="opacity:1;" data-delay="5000" data-time="1000" data-easing="easeInOutQuart">
         <h3 class="tit"></h3><p class="text"></p>
     </div>
@@ -46,6 +50,18 @@
     <div class="scheme_imgright scheme_jia" data-style="opacity:0;" data-animate="opacity:1;" data-delay="1600" data-time="300" data-easing="linear">
         <h3 class="tit"></h3><p class="text"></p>
     </div>
+    <div class="scheme_imgright scheme_intro1" data-style="opacity:0;left:40%;" data-animate="opacity:1;left:50%;" data-delay="1100" data-time="300" data-easing="linear">
+        <h3 class="tit"></h3><p class="text scheme_introtxt">修红舒润面膜可作为紧急修红的急救面膜使用。在洁面之后均匀涂抹于面部，待皮肤吸收10分钟后,用化妆棉将剩余面膜拭去。配方精简，仅含<span>11种</span>成分。</p>
+    </div>
+    <div class="scheme_imgright scheme_intro2" data-style="opacity:0;left:40%;" data-animate="opacity:1;left:50%;" data-delay="1800" data-time="300" data-easing="linear">
+        <h3 class="tit"></h3><p class="text scheme_introtxt">使用面膜之后，喷洒具有舒缓功效的雅漾活泉水，为肌肤补充活性水分子的同时，舒缓肌肤，缓解可能存在的敏感泛红。</p>
+    </div>
+    <div class="scheme_imgright scheme_intro3" data-style="opacity:0;left:60%;" data-animate="opacity:1;left:50%;" data-delay="2700" data-time="300" data-easing="linear">
+        <h3 class="tit"></h3><p class="text scheme_introtxt">对于耐受性较差的肌肤来说,洁面的选择也非常关键。应选择质地温和的抗红产品,同时可配合手指肚轻轻按压的手法,以促进血液流动。</p>
+    </div>
+    <div class="scheme_imgright scheme_intro4" data-style="opacity:0;left:60%;" data-animate="opacity:1;left:50%;" data-delay="3200" data-time="300" data-easing="linear">
+        <h3 class="tit"></h3><p class="text scheme_introtxt">天然修护保湿因子与雅漾活泉水协同作用，舒缓、保湿，缓解皮肤不适现象。质地润泽，含白凡士林和植物性角鲨烷，有效修复水脂膜，重建皮肤保护屏障。</p>
+    </div>
     <div class="scheme_imgright scheme_product1" data-style="opacity:0;left:40%;" data-animate="opacity:1;left:50%;" data-delay="1300" data-time="300" data-easing="linear">
         <h3 class="tit"></h3><p class="text"></p>
     </div>
@@ -62,18 +78,6 @@
         <h3 class="tit"></h3><p class="text"></p>
     </div>
     <div class="scheme_imgright scheme_product4" data-style="opacity:0;left:60%;" data-animate="opacity:1;left:50%;" data-delay="3100" data-time="300" data-easing="linear">
-        <h3 class="tit"></h3><p class="text"></p>
-    </div>
-    <div class="scheme_imgright scheme_intro1" data-style="opacity:0;left:40%;" data-animate="opacity:1;left:50%;" data-delay="1100" data-time="300" data-easing="linear">
-        <h3 class="tit"></h3><p class="text"></p>
-    </div>
-    <div class="scheme_imgright scheme_intro2" data-style="opacity:0;left:40%;" data-animate="opacity:1;left:50%;" data-delay="1800" data-time="300" data-easing="linear">
-        <h3 class="tit"></h3><p class="text"></p>
-    </div>
-    <div class="scheme_imgright scheme_intro3" data-style="opacity:0;left:60%;" data-animate="opacity:1;left:50%;" data-delay="2700" data-time="300" data-easing="linear">
-        <h3 class="tit"></h3><p class="text"></p>
-    </div>
-    <div class="scheme_imgright scheme_intro4" data-style="opacity:0;left:60%;" data-animate="opacity:1;left:50%;" data-delay="3200" data-time="300" data-easing="linear">
         <h3 class="tit"></h3><p class="text"></p>
     </div>
     <!--  -->
